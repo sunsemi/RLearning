a30e07b5a6f03d20aba5b4c94cae3605b3c2dc0d
ykchoi <ykchoi@sunsemico.com>
2025-03-15 14:59:26 +0900

    aa

---
+++ VarTest.py
@@ -0,0 +1,9 @@
+def main():
+    var_a=100
+    print("var_a")
+    print(100+200)
+    print(var_a, end="------")
+    print("Var_a:    ",   var_a,sep="***\n")
+
+if __name__=="__main__":
+    main()
